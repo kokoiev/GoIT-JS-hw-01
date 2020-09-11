67dffefcfc28bcf1f041f73c8bb1c5faff5b81f6
sqralan <sqralan@ukr.net>
2020-09-11 07:31:44 +0300

    alert now works in 6_hometask

--- task06.js
+++ task06.js
@@ -6,7 +6,15 @@ do {
   input = prompt(`Введите число`);
   console.log(input);
 
+  const numInput = Number.isNaN(Number(input));
+  console.log(numInput);
+  if (numInput === true) {
+    alert(`${input} Это не число, попробуй ввести число`);
+    break;
+  }
+
   total = total + +input;
 } while (input !== null);
 
 console.log(`Общая сумма чисел равна ${total}`);
+2;
